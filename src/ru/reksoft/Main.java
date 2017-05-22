96ba635dfdd563a59ae66f66b4726a76b3cb8133
agubanov <arteumg@mail.ru>
2017-05-22 12:30:12 +0300

    test changes

--- src/ru/reksoft/Main.java
+++ src/ru/reksoft/Main.java
@@ -1,8 +1,10 @@
 package ru.reksoft;
 
+import javax.xml.transform.sax.SAXSource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello");
     }
 }
