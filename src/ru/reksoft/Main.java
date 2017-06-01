ad704965c7a8298e67933a0d78be59864385637f
agubanov <arteumg@mail.ru>
2017-06-01 14:47:49 +0300

    Изменен README.md

--- src/ru/reksoft/Main.java
+++ src/ru/reksoft/Main.java
@@ -5,6 +5,6 @@ import javax.xml.transform.sax.SAXSource;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hellffo");
     }
 }
